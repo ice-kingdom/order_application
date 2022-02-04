c231da75052ecaa015df10e353ee085d6d1d9156
Thomas <test@test.ru>
2022-02-04 21:55:36 +0500

    мелочь

--- config/routes.php
+++ config/routes.php
@@ -8,11 +8,12 @@
 #  controller: App\Controller\TestController::index*/
 use App\Controller\TestController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use \App\Controller\AuthController;
 
 return function (RoutingConfigurator $routes) {
     $routes->add('test', '/test')
         // значение контроллера ммеет формат [controller_class, method_name]
         ->controller([TestController::class, 'index'])->methods(['GET']);
     $routes->add('auth', '/auth')
-        ->controller([\App\Controller\AuthController::class, 'index'])->methods(['GET']);
+        ->controller([AuthController::class, 'index'])->methods(['GET']);
 };
